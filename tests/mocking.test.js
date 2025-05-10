ea1af380843db1917ad4dbb3fb60146affb2a0d1
Shukhratullo Nurmakhammatov <shuhratillon@gmail.com>
2025-05-10 12:51:58 +0500

    test: add unit tests for getPriceInCurrency and mock exchange rate functionality

---
+++ tests/mocking.test.js
@@ -0,0 +1,27 @@
+import { vi, it, expect, describe } from "vitest";
+import { getPriceInCurrency } from "../src/mocking";
+import { getExchangeRate } from "../src/libs/currency";
+
+vi.mock("../src/libs/currency");
+
+describe("test suite", () => {
+  it("test case", () => {
+    const sendText = vi.fn();
+    sendText.mockReturnValue("ok");
+
+    const result = sendText("message");
+
+    expect(sendText).toHaveBeenCalledWith("message");
+    expect(result).toBe("ok");
+  });
+});
+
+describe("getPriceInCurrency", () => {
+  it(" return price in target currency", () => {
+    vi.mocked(getExchangeRate).mockReturnValue(1.5);
+
+    const price = getPriceInCurrency(10, "AUD");
+
+    expect(price).toBe(15);
+  });
+});
