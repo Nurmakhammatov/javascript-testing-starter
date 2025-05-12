f326e637787ec253097dd7bf4a738f792e08a39b
Shukhratullo Nurmakhammatov <shuhratillon@gmail.com>
2025-05-12 11:40:45 +0500

    test: add unit tests for getShippingInfo, renderPage, and submitOrder functions

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -1,8 +1,19 @@
 import { vi, it, expect, describe } from "vitest";
-import { getPriceInCurrency } from "../src/mocking";
+import {
+  getPriceInCurrency,
+  getShippingInfo,
+  renderPage,
+  submitOrder,
+} from "../src/mocking";
 import { getExchangeRate } from "../src/libs/currency";
+import { getShippingQuote } from "../src/libs/shipping";
+import { trackPageView } from "../src/libs/analytics";
+import { charge } from "../src/libs/payment";
 
 vi.mock("../src/libs/currency");
+vi.mock("../src/libs/shipping");
+vi.mock("../src/libs/analytics");
+vi.mock("../src/libs/payment");
 
 describe("test suite", () => {
   it("test case", () => {
@@ -17,11 +28,74 @@ describe("test suite", () => {
 });
 
 describe("getPriceInCurrency", () => {
-  it(" return price in target currency", () => {
+  it("should return price in target currency", () => {
     vi.mocked(getExchangeRate).mockReturnValue(1.5);
 
     const price = getPriceInCurrency(10, "AUD");
 
     expect(price).toBe(15);
+  });
+});
+
+describe("getShippingInfo", () => {
+  it("should return shipping unavailable if quaote cannot be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue(null);
+
+    const result = getShippingInfo("London");
+
+    expect(result).toMatch(/unavailable/i);
+  });
+
+  it("should return shipping info if quote can be fetched", () => {
+    vi.mocked(getShippingQuote).mockReturnValue({ cost: 10, estimatedDays: 2 });
+
+    const result = getShippingInfo("London");
+
+    expect(result).toMatch("$10");
+    expect(result).toMatch(/2 days/i);
+    expect(result).toMatch(/shipping cost: \$10 \(2 days\)/i);
+  });
+});
+
+describe("renderPage", () => {
+  it("should return correct content", async () => {
+    const result = await renderPage();
+
+    expect(result).toMatch(/content/i);
+  });
+
+  it("should call analytics", async () => {
+    await renderPage();
+
+    expect(trackPageView).toHaveBeenCalledWith("/home");
+  });
+});
+
+describe("submitOrder", () => {
+  const order = { totalAmount: 10 };
+  const creditCard = { creditCardNumber: "1234" };
+
+  it("should charge customer", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "success" });
+
+    await submitOrder(order, creditCard);
+
+    expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount);
+  });
+
+  it("should return succes when payment is successfull", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "success" });
+
+    const result = await submitOrder(order, creditCard);
+
+    expect(result).toEqual({ success: true });
+  });
+
+  it("should return error when payment is failed", async () => {
+    vi.mocked(charge).mockResolvedValue({ status: "failed" });
+
+    const result = await submitOrder(order, creditCard);
+
+    expect(result).toEqual({ success: false, error: "payment_error" });
   });
 });
